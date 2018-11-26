3d5e088a4ae6343e28639e0777c8a9777586054a
andreadidio98 <andreadidio98@gmail.com>
2018-11-26 19:30:53 +0100

    TODO:add comments

--- Assignment 2/vault.py
+++ Assignment 2/vault.py
@@ -50,23 +50,18 @@ class Encryption:
     def __init__(self, iv, file, key):
         self.filename = file
         self.init_vector = iv
-        while len(key) != 32:
-            key += '0'
-        self.secret_key = key
-        #print(self.secret_key, len(self.secret_key))
+        self.secret_key = SHA256.new(key).digest()
+
 
     def padPlain(self):
         fd = open(self.filename, 'rb')
         plain = fd.read()
         if len(plain) % 16 != 0:
-            # for i in range(0, (AES.block_size - (len(plain) % AES.block_size))):
-            #     plain += chr((AES.block_size - (len(plain) % AES.block_size)))
             self.padded = plain + chr((AES.block_size - (len(plain) % AES.block_size))) * (AES.block_size - (len(plain) % AES.block_size))
         return self.padded
 
     def encrypt(self):
         cipher = AES.new(self.secret_key, AES.MODE_CBC, self.init_vector)
-        fd = open(self.filename, 'rb')
         encrypted = self.init_vector + cipher.encrypt(self.padPlain())
         return encrypted
 
@@ -75,9 +70,6 @@ class Encryption:
         s = fd.read()
         self.init_vector = s[:AES.block_size]
         cipher = AES.new(self.secret_key, AES.MODE_CBC, self.init_vector)
-
-        #print(s,len(s))
-
         decrypted = cipher.decrypt(s[AES.block_size:])
         data = decrypted[:-ord(decrypted[-1:])]
         return data
@@ -106,45 +98,21 @@ elif sys.argv[1] == '-v':
         sys.exit(0)
     else:
         sys.exit(1)
-    #print(sign.verifyFile(pub_key, signature)) #TODO: Return 0 if success or 1 if not...don't print bool
 elif sys.argv[1] == '-e':
-    if sys.argv[3][:2] == '0x':
-        secret_key = binascii.unhexlify(sys.argv[3][2:])
-    else:
-        secret_key = binascii.unhexlify(sys.argv[3])
     if sys.argv[4][:2] == '0x':
         init_vector = binascii.unhexlify(sys.argv[4][2:])
     else:
         init_vector = binascii.unhexlify(sys.argv[4])
-    #print(init_vector)
-    #print(len(init_vector))
-    #print(len("A"*16), len("B"*16))
+    secret_key = sys.argv[3]
     enc = Encryption(init_vector, file_path, secret_key)
-    #print(len(enc.init_vector))
-    # li = map(ord, enc.encrypt())
-    # #l = map(str, li)
-    # s = ''
-    # for i in li:
-    #     s += "{0:b}".format(i)
-    # print(s)
-    print(enc.encrypt(),end='')
-    # print()
-    # print(enc.decrypt('./sign')[16:], len(enc.decrypt('./sign')[16:]))
-    # if (enc.encrypt() == enc.decrypt('./sign')):
-    #     print(True)
-    # else:
-    #     print(False)
+    print(enc.encrypt(), end="")
 elif sys.argv[1] == '-d':
-    if sys.argv[3][:2] == '0x':
-        secret_key = binascii.unhexlify(sys.argv[3][2:])
-    else:
-        secret_key = binascii.unhexlify(sys.argv[3])
     if sys.argv[4][:2] == '0x':
         init_vector = binascii.unhexlify(sys.argv[4][2:])
     else:
         init_vector = binascii.unhexlify(sys.argv[4])
-    #print(secret_key, init_vector)
+    secret_key = sys.argv[3]
     dec = Encryption(init_vector, file_path, secret_key)
-    print(dec.decrypt(file_path))
+    print(dec.decrypt(file_path), end="")
 else:
     print("Error: Unknown Command!")
